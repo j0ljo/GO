c7afecedc073b4d1458d503f53580c297c8adbe3
Joel Joe <joeljoe0726@gmail.com>
2026-05-28 11:01:33 +0530

    added a sub-directory here

---
+++ Simple_Projects__GO/Simple_Calculator/calc.go
@@ -0,0 +1,39 @@
+//
+//
+package main 
+import "fmt"
+
+func main() {
+	var num1, num2 float64 
+	var operator string 
+
+	fmt.Println("Enter the first number: ")
+	fmt.Scanln(&num1)
+	fmt.Println("Enter the second number: ")
+	fmt.Scanln(&num2)
+	fmt.Print("Enter the operation + - * / ")
+	fmt.Scanln(&operator)
+
+	switch operator{
+		case "+":
+			fmt.Printf("%f %s %f = %f", num1,operator,num2, num1+num2)
+		
+		case "-":
+			fmt.Printf("%f %s %f = %f", num1,operator,num2, num1-num2)
+
+		case "*":
+			fmt.Printf("%f %s %f = %f", num1,operator,num2,num1*num2)
+		
+		case "/":
+			if num2 == 0.0 {
+				fmt.Println("Invalid operation")
+			} else{
+				fmt.Printf("%f %s %f = %f",num1,operator,num2,num1/num2)
+				}
+		default:
+			fmt.Println("Invalid operator")
+			
+		}
+
+	
+}
